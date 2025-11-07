6cfa0681ea723952ad0008d7e74ee90cf9a8a07c
donthireddybhargavi69-dev <donthireddybhargavi69@gmail.com>
2025-11-07 14:27:07 +0530

    Update urls.py

--- Donotech/urls.py
+++ Donotech/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import StaticViewSitemap, BlogPostSitemap, CourseSitemap
+sitemaps = {
+    'static': StaticViewSitemap,
+    'blog': BlogPostSitemap,
+    'courses': CourseSitemap,
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('',include('Donotechapp.urls')),
 ]
 if settings.DEBUG:
