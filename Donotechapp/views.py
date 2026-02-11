285a56afe5d27553791d1c9aa37bf036142ae4ef
anduriramreddy-cmd <anduriramreddy@gmail.com>
2026-02-11 13:16:07 +0530

    Update views.py

--- Donotechapp/views.py
+++ Donotechapp/views.py
@@ -350,3 +350,7 @@ def contact_success(request):
     return render(request, 'contact-success.html')
 
 
+from django.shortcuts import render
+
+def privacy_policy(request):
+    return render(request, 'privacy_policy.html')
